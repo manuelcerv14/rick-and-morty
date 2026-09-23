c4047e57b1a5ca11ef9976f0ea25f28da6fa0e45
Man <german24491@cbtis75.edu.mx>
2026-09-23 08:12:33 -0600

    mi parte del service.ts

---
+++ src/app/services/metodos 7 y 8 pal service.ts
@@ -0,0 +1,9 @@
+ // 7. Obtener todos los episodios
+  obtenerEpisodios(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/episode`);
+  }
+
+  // 8. Obtener un episodio por ID
+  obtenerEpisodio(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/episode/${id}`);
+  }
